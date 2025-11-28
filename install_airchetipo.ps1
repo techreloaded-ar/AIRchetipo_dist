44249c6da67f569a090c6951dd8b3be9d9772de3
Stefano Marello <smarello89@gmail.com>
2025-11-28 09:43:30 +0100

    Update link inside install_airchetipo.ps1

--- install_airchetipo.ps1
+++ install_airchetipo.ps1
@@ -1,5 +1,5 @@
 try {
-    Invoke-WebRequest -Uri "https://github.com/opencode-ai/opencode/archive/refs/heads/main.zip" -OutFile "main.zip" -ErrorAction Stop
+    Invoke-WebRequest -Uri "https://github.com/techreloaded-ar/AIRchetipo_dist/archive/refs/heads/main.zip" -OutFile "main.zip" -ErrorAction Stop
     Expand-Archive -Path main.zip -DestinationPath . -Force
     Remove-Item main.zip
 } catch {
