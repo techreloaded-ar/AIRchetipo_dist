b41cf00562b7947f97cbec34388da164501695d3
Stefano Marello <smarello89@gmail.com>
2025-11-28 09:51:47 +0100

    add project param to install_airchetipo.ps1

--- install_airchetipo.ps1
+++ install_airchetipo.ps1
@@ -1,6 +1,11 @@
+param(
+    [string]$Project
+)
+
 try {
     Invoke-WebRequest -Uri "https://github.com/techreloaded-ar/AIRchetipo_dist/archive/refs/heads/main.zip" -OutFile "main.zip" -ErrorAction Stop
     Expand-Archive -Path main.zip -DestinationPath . -Force
+	Rename-Item -Path AIRchetipo_dist-main -NewName $Project
     Remove-Item main.zip
 } catch {
     Write-Host "Errore nel download: $_"
